191c434c65888887bc7593d151574acfd5062480
Lívia Orsós <livia.orsos@codesharp.hu>
2021-08-25 17:02:18 +0200

    highlight error

--- src/components/CodeMirror.vue
+++ src/components/CodeMirror.vue
@@ -6,7 +6,7 @@
 
 <script>
 import { defineComponent, ref } from 'vue';
-import { prettyPrint } from '../utils/PrettyPrint';
+import { prettyPrint, validateJson } from '../utils/PrettyPrint';
 import CodeMirror from 'codemirror/lib/codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/javascript/javascript.js';
@@ -14,24 +14,19 @@ import 'codemirror/mode/javascript/javascript.js';
 export default defineComponent({
   name: 'CodeMirror',
   setup() {
-    const editor = ref(null)
-    return { editor }
+    const editor = ref(null);
+    const cmError = ref({});
+    return { editor, cmError }
   },
   mounted(){
-    let func = this.debounce(function(cm) {
-      const oldValue = cm.getValue()
-      const newValue = prettyPrint(cm.getValue());
-      if (oldValue != newValue) {
-        cm.setValue(newValue);
-      }
-    }, 1000);
+    let debouncedCheckJson = this.debounce(this.checkJson, 1000);
     this.editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
       mode: "text/javascript",
       lineNumbers: true,
       autoCloseTags: true,
       tabSize: 2,
     }).on('change', cm => {
-      func(cm);
+      debouncedCheckJson(cm);
     });
   },
   methods: {
@@ -45,6 +40,37 @@ export default defineComponent({
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
       };
+    },
+    lineToColor(line, color){
+      const list = document.getElementsByClassName('CodeMirror-linenumber');
+        for (let i = 0; i < list.length; i++) {
+          const lineNumberElement = list[i];
+          const textVal = lineNumberElement.textContent;
+          if (textVal) {
+            const lineNum = parseInt(textVal, 10);
+            if (line + 1 === lineNum) {
+              const lineElement = lineNumberElement.parentElement?.parentElement?.querySelector('.CodeMirror-line');
+              if (lineElement)
+                lineElement.setAttribute('style', `background-color:${color};`);
+            }
+          }
+        }
+    },
+    unsetHighlight: function (){
+      const e  = document.getElementsByClassName('CodeMirror-line');
+      for(let i = 0; i < e.length; i++){
+        e[i].setAttribute('style', 'background-color: unset;');
+      }
+    },
+    checkJson(cm) {
+      const oldValue = cm.getValue();
+      const newValue = prettyPrint(cm.getValue());
+        this.unsetHighlight()
+      if(validateJson(newValue).error){
+        this.lineToColor(validateJson(newValue).line, 'red');
+      }else if (oldValue != newValue) {
+        cm.setValue(newValue);
+      }
     }
   }
 });
