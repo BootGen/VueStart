900ff09653cc3d9636f59c226686741f45f76022
Lívia Orsós <livia.orsos@codesharp.hu>
2021-09-07 09:34:14 +0200

    landing page style

--- ClientApp/src/views/LandingPage.vue
+++ ClientApp/src/views/LandingPage.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="container-fluid mx-auto">
-    <div class="d-inline-flex align-center justify-content-center">
-        <div class="row">
-          <h1>Start Vue</h1>
-          <p class="lead">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-          </p>
-          <router-link class="pl-2" to="/editor">Editor</router-link>
-        </div>
-        <vueuen type="default"></vueuen>
+  <div class="container-fluid d-inline-flex h-100 align-items-center m-3">
+    <div class="row">
+      <h1>Start Vue</h1>
+      <p class="lead text-justify">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+      </p>
+      <router-link class="p-2 btn btn-outline-primary" to="/editor">Go to editor</router-link>
     </div>
+    <vueuen type="default"></vueuen>
   </div>
 </template>
 
