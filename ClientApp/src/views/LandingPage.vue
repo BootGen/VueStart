2638191f52ead5cd6e718fa96691a1a4c557e92e
Lívia Orsós <livia.orsos@codesharp.hu>
2021-09-09 11:40:09 +0200

    anim 1

--- ClientApp/src/views/LandingPage.vue
+++ ClientApp/src/views/LandingPage.vue
@@ -1,32 +1,68 @@
 <template>
   <div class="container h-100">
-    <div class="row h-100 justify-content-center align-items-center">
-      <div class="col-12">
-        <div class="row">
+    <transition name="fade">
+      <div class="row justify-content-center align-items-center" :class="{ 'shrink' : showNav, 'toShrink': ! showNav }">
+        <div class="col-6">
           <h1>Start Vue!</h1>
-          <p class="lead text-justify">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-          </p>
-          <router-link class="btn btn-outline-primary rounded-pill" to="/editor">Go!</router-link>
+          <div>
+            <p class="lead text-justify">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            </p>
+            <button class="btn btn-outline-primary rounded-pill" @click="clickBtn()">Go!</button>
+          </div>
+        </div>
+        <div class="col-6">
+          <vueuen type="default"></vueuen>
         </div>
       </div>
-      <vueuen type="default"></vueuen>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import { defineComponent, ref } from 'vue';
 import Vueuen from '../components/Vueuen.vue'
 
-export default {
+export default defineComponent({
   components: { Vueuen },
   name: 'LandingPage',
-}
+  setup() {
+    const showNav = ref(false);
+    return { showNav }
+  },
+  methods: {
+    clickBtn() {
+      console.log(this.showNav)
+      this.showNav = !this.showNav;
+      console.log(this.showNav)
+    }
+  }
+});
+
 </script>
 
 <style scoped>
-  .btn{
-    width: max-content;
+  .btn {
     font-size: 1.5em;
   }
+  .toShrink {
+    height: 100%;
+  }
+  .toGrow {
+    height: 45%;
+  }
+  .shrink {
+    animation-name: shrink;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+    animation-direction: alternate;
+  }
+  @keyframes shrink {
+  from {
+    height: 100%;
+    }
+  to {
+    height: 45%;
+    }
+}
 </style>
