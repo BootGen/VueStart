d56877a4c8a713614a7e6af28f6d904d9c9c80c0
Gabor Angyal <gabor.angyal@codesharp.hu>
2021-09-15 17:08:22 +0200

    init generation

---
+++ Server/Controllers/GenerationController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BootGen;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text.Json;
+
+namespace StartVue.Controllers
+{
+    [ApiController]
+    [Route("generate")]
+    public class GenerationController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Generate([FromBody] JsonElement json)
+        {
+            var dataModel = new DataModel();
+            var jObject = JObject.Parse(json.ToString(), new JsonLoadSettings { CommentHandling = CommentHandling.Ignore, DuplicatePropertyNameHandling = DuplicatePropertyNameHandling.Error });
+            dataModel.Load(jObject);
+            var collection = new ResourceCollection(dataModel);
+            var seedStore = new SeedDataStore(collection);
+            seedStore.Load(jObject);
+
+            const string targetFolder = "../Generated";
+            var disk = new Disk(targetFolder);
+            
+            foreach( var file in Load("../BootgenPlugin/files").Files) {
+                var dir = Path.Combine(targetFolder, file.Path);
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+                System.IO.File.WriteAllText(Path.Combine(dir, file.Name), file.Content);
+            }
+            var project = new ClientProject
+            {
+                Config = new ClientConfig {
+                    ComponentExtension = "vue",
+                    Extension = "js"
+                },
+                Disk = disk,
+                ResourceCollection = collection,
+                SeedStore = seedStore,
+                Templates = Load("../BootgenPlugin/templates")
+            };
+            project.GenerateFiles("Dummy", "http://localhost:5000");
+            return Ok(project);
+        }
+
+        private static VirtualDisk Load(string path)
+        {
+            var templates = new VirtualDisk();
+            foreach (var file in Directory.EnumerateFiles(path))
+            {
+                templates.Files.Add(new VirtualFile
+                {
+                    Name = Path.GetFileName(file),
+                    Path = "",
+                    Content = System.IO.File.ReadAllText(file)
+                });
+            }
+
+            return templates;
+        }
+    }
+}
