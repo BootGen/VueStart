163cd79dee8263134f0f8e9c3bfbf2eb62d51970
Gabor Angyal <gabor.angyal@codesharp.hu>
2021-09-15 16:31:27 +0200

    dotnet docker setup

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -44,7 +44,7 @@ namespace StartVue
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StartVue v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@ nginx
 cd /code/ClientApp
 npm run serve --  --mode production &
 cd /code/DefaultSite
-npm run serve -- --port 8090  --mode production
+npm run serve -- --port 8090  --mode production &
+cd /code/Server
+/root/.dotnet/dotnet run
