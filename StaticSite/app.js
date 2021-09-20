019acbaac7f31db13afb22828687c3645b501c3f
Gábor Angyal <gabor.angyal@codesharp.hu>
2021-09-20 21:03:21 +0200

    add static site

---
+++ StaticSite/app.js
@@ -0,0 +1,14 @@
+const Counter = {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    mounted() {
+      setInterval(() => {
+        this.counter++
+      }, 1000)
+    }
+  }
+  
+  Vue.createApp(Counter).mount('#app')
