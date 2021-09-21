e6cac613b35e43a62f4881f049e4c78132dd0bc0
Gábor Angyal <gabor.angyal@codesharp.hu>
2021-09-21 07:54:43 +0200

    static pet view

--- StaticSite/app.js
+++ StaticSite/app.js
@@ -1,14 +1,49 @@
-const Counter = {
+const  app = Vue.createApp({
     data() {
       return {
-        counter: 0
+        garfield: {
+          name: 'Garfield',
+          species: 'Cat'
+        }
       }
-    },
-    mounted() {
-      setInterval(() => {
-        this.counter++
-      }, 1000)
     }
-  }
+  });
+
+  app.component('pet-view', {
+    template: `
+    <li class="list-group-item">
+    <button type="button" class="btn btn-default btn-sm" @click="$emit('edit')">
+      <span class="bi bi-pencil" aria-hidden="true"></span> Edit
+    </button>
+    <button type="button" class="btn btn-default btn-sm" @click="$emit('delete')">
+      <span class="bi bi-trash" aria-hidden="true"></span> Delete
+    </button>
+  </li>
+  <li class="list-group-item">
+    <div class="row">
+      <div class="col-4 d-flex justify-content-start text-break text-start">
+        Name:
+      </div>
+      <div class="col-8 d-flex justify-content-start text-break">
+        {{ modelValue.name }}
+      </div>
+    </div>
+  </li>
+  <li class="list-group-item">
+    <div class="row">
+      <div class="col-4 d-flex justify-content-start text-break text-start">
+        Species:
+      </div>
+      <div class="col-8 d-flex justify-content-start text-break">
+        {{ modelValue.species }}
+      </div>
+    </div>
+  </li>`,
+    name: 'PetView',
+    props: {
+      modelValue: Object,
+    },
+    emits: ['edit',  'delete']
+  });
   
-  Vue.createApp(Counter).mount('#app')
+  app.mount('#app');
