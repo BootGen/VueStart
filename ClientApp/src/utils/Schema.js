0e741d1fb884e7294c77e8d7aec70e8970db8f2d
Lívia Orsós <livia.orsos@codesharp.hu>
2021-12-16 10:20:30 +0100

    #SV-49 fix schema error

--- ClientApp/src/utils/Schema.js
+++ ClientApp/src/utils/Schema.js
@@ -1,44 +1,89 @@
 'use strict';
 
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+function merge(current, updates) {
+  for (key of Object.keys(updates)) {
+    if (!current.hasOwnProperty(key) || typeof updates[key] !== 'object') current[key] = updates[key];
+    else merge(current[key], updates[key]);
+  }
+  return current;
+}
+function mergeDeep(target, source) {
+  const isObject = (obj) => obj && typeof obj === 'object';
+
+  if (!isObject(target) || !isObject(source)) {
+    return source;
+  }
+
+  Object.keys(source).forEach(key => {
+    const targetValue = target[key];
+    const sourceValue = source[key];
+    if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+      target[key] = targetValue.concat(sourceValue);
+    } else if (isObject(targetValue) && isObject(sourceValue)) {
+      target[key] = mergeDeep(Object.assign({}, targetValue), sourceValue);
+    } else {
+      target[key] = sourceValue;
+    }
+  });
+
+  return target;
+}
+
 function typeArray(val) {
   let r = {'type': 'object', 'properties': {}}
   val.forEach( function (v) {
-    Object.assign(r.properties, typeValue(v).properties)
+    r = mergeDeep(r, typeValue(v).properties)
+    
   })
   return r;
 }
 
 
 function typeValue(val) {
+  let type = [];
   if (Array.isArray(val)) {
-    return {'type': 'array', 'items': typeArray(val)};
+    type.push({array: true});
+  }
+  
+  if (typeof val == 'object') {
+    type.push({object: true});
+  }
+  
+  if (typeof val == 'number' && Number.isInteger(val)) {
+    type.push({integer: true});
+  }
+  type.push({type: typeof val})
+
+  if (Array.isArray(val)) {
+    return {'type': type, 'items': typeArray(val)};
   }
   
   if (typeof val == 'object') {
     let properties = getProperties(val);
-    return {'type': 'object', 'properties': properties};
+    return {'type': type, 'properties': properties};
   }
   
   if (typeof val == 'number' && Number.isInteger(val)) {
-    return {'type': 'integer'}
+    return {'type': type}
   }
 
-  return { 'type': typeof val };
+  return { 'type': type };
 }
 
 function getProperties(j) {
   let k = Object.keys(j);
-  
   k.forEach(function(name) {
     j[name] = typeValue(j[name]);
   })
-  
   return j;
 }
 
 export function getSchema(json_object) {
   let schema = {};
-  
   schema['properties'] = getProperties(json_object);
 
   return schema;
