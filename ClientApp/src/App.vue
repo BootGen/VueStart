d1494af7b12bb27420066dbb130e57c21729570c
Lívia Orsós <livia.orsos@codesharp.hu>
2021-12-16 11:39:38 +0100

    #SV-51 fix Vuecoon size in landing

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -694,7 +694,10 @@ body {
       text-align: center;
     }
     .vuecoon.landing {
-      max-width: 200px;
+      max-width: 35vw;
+    }
+    .text-justify{
+      font-size: 1rem;
     }
     .jumbo-text-full.content {
       max-width: 50%;
