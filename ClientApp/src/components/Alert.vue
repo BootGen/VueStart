9b5ac8dae9bbec54d1eb5b82497d6597bb805722
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-01-12 18:01:06 +0100

    update alert design

--- ClientApp/src/components/Alert.vue
+++ ClientApp/src/components/Alert.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="col-12 d-flex error-alert-container">
-    <div class="alert m-2 alert-danger d-flex align-items-center">
-      <div>
-        {{ errorMessage }}
-        <button type="button" class="btn fix-btn" aria-label="Fix" @click="$emit('fixData')" v-if="isFixable">fix</button>
+    <div class="alert m-2 alert-warning d-flex align-items-center">
+      <div class="text-center">
+        {{ lastErrorMessage }} <br>
+        <button type="button" class="btn btn-warning" aria-label="Fix" @click="$emit('fixData')" v-if="isFixable">Fix it!</button>
       </div>
       <button type="button" class="btn p-2" aria-label="Close" @click="$emit('close')"><span class="bi bi-x-lg px-2" aria-hidden="true"></span></button>
     </div>
@@ -20,13 +20,13 @@ export default defineComponent({
   },
   emits: ['close', 'fixData'],
   setup(props) {
-    let errorMessage = ref(props.errorMessage)
+    let lastErrorMessage = ref(props.errorMessage)
     watchEffect(() => {
       if(props.errorMessage) {
-        errorMessage.value = props.errorMessage;
+        lastErrorMessage.value = props.errorMessage;
       }
     });
-    return { errorMessage }
+    return { lastErrorMessage }
   }
 });
 </script>
@@ -37,15 +37,6 @@ export default defineComponent({
     bottom: 0;
     justify-content: center!important;
     z-index: 999;
-  }
-  .fix-btn {
-    color: #842029;
-    text-decoration: underline;
-    padding: unset;
-    padding-left: 0.5rem;
-  }
-  .fix-btn:hover {
-    color: #b42733;
   }
   @media (max-width: 992px) {
     .error-alert-container {
