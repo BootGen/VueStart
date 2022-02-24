db1b3d0fa70984e066b5db1b39be46a1edc1ede0
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-02-24 07:25:01 +0100

    add update statistics migration

--- Server/Migrations/20220222180238_UpdateStatistics.Designer.cs
+++ Server/Migrations/20220222180238_UpdateStatistics.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VueStart;
 
@@ -10,9 +11,10 @@ using VueStart;
 namespace VueStart.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220222180238_UpdateStatistics")]
+    partial class UpdateStatistics
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
