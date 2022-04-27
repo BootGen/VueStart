92dc91923fe640a3d5037248f65fa63534e25de7
Lívia Orsós <livia.orsos@codesharp.hu>
2022-04-27 09:33:01 +0200

    #SV-152 create AdminController class

---
+++ Server/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text.Json;
+using System.IO.Compression;
+using VueStart.Services;
+using VueStart.Data;
+
+namespace VueStart.Controllers
+{
+    [ApiController]
+    [Route("api/admin")]
+    public class AdminController : ControllerBase
+    {
+        
+    }
+}
