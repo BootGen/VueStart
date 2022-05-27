49908c87bd49cdfac501a4834c1969cd14506cce
Lívia Orsós <livia.orsos@codesharp.hu>
2022-05-27 16:33:25 +0200

    #SV-181 ShareableLinkSettings migration

--- Server/Migrations/20220527143241_ShareableLinkSettings.Designer.cs
+++ Server/Migrations/20220527143241_ShareableLinkSettings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VueStart;
@@ -12,9 +13,10 @@ using VueStart;
 namespace VueStart.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220527143241_ShareableLinkSettings")]
+    partial class ShareableLinkSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Server/Migrations/20220527143241_ShareableLinkSettings.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VueStart.Migrations
+{
+    public partial class ShareableLinkSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "ShareableLinks");
+
+            migrationBuilder.DropColumn(
+                name: "Editable",
+                table: "ShareableLinks");
+
+            migrationBuilder.DropColumn(
+                name: "FrontendType",
+                table: "ShareableLinks");
+
+            migrationBuilder.RenameColumn(
+                name: "Json",
+                table: "ShareableLinks",
+                newName: "GenerateRequest");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEIyo9Z9EEDv7Dmyv5tqUNQ6pgWN6HV2kzycBoFanFRBjNxClWbdgQe2BSCq5ldV4Mw==");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GenerateRequest",
+                table: "ShareableLinks",
+                newName: "Json");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "ShareableLinks",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Editable",
+                table: "ShareableLinks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrontendType",
+                table: "ShareableLinks",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEP2ySbRDxPLVAzU13JfKBPpxhk2Vjh0LzEe29VP+MuGnKdzeD8BuGR5Dd1oo7gFzzA==");
+        }
+    }
+}
